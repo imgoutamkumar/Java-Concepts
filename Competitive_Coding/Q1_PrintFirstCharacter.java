0de210d97d4e582e55f3dc682a5777918258aba9
Goutam Kumar <goutamkumar42010no@gmail.com>
2025-01-05 10:41:05 +0530

    some minor changes

--- Competitive_Coding/Q1_PrintFirstCharacter.java
+++ Competitive_Coding/Q1_PrintFirstCharacter.java
@@ -4,11 +4,13 @@ package Competitive_Coding;
 public class Q1_PrintFirstCharacter {
     public static void main(String[] args) {
 
+        double now = System.currentTimeMillis();
         String str = "My name is khan";
         String word[] = str.split(" ");
 
         for (int i = 0; i < word.length; i++) {
             System.out.println(word[i].charAt(0));
         }
+        System.out.println("time taken : " + (System.currentTimeMillis() - now) + "millisecs");
     }
 }
